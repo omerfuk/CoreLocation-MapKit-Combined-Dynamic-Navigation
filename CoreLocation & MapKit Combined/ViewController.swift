d35daf623b1fbb0c519d267e572b168cb17729f2
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 13:59:42 +0300

    .

--- CoreLocation & MapKit Combined/ViewController.swift
+++ CoreLocation & MapKit Combined/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hizLabel: UILabel!
+    @IBOutlet weak var boylamLabel: UILabel!
+    @IBOutlet weak var enlemLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager:CLLocationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
 }
 
+extension ViewController:CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let sonKonum:CLLocation = locations[locations.count - 1]
+        
+        enlemLabel.text = "Enlem \(sonKonum.coordinate.latitude)"
+        boylamLabel.text = "Boylam \(sonKonum.coordinate.longitude)"
+        hizLabel.text = "Hız \(sonKonum.speed)"
+        
+        
+        let konum = CLLocationCoordinate2D(latitude: sonKonum.coordinate.latitude, longitude: sonKonum.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
+        let bolge = MKCoordinateRegion(center: konum, span: span)
+        
+        mapView.setRegion(bolge, animated: true)
+        
+        mapView.showsUserLocation = true
+    }
+}
